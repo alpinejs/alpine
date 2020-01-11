b8f4ce1d725805f94eb76a346e506e465c0198d7
Keyur Shah <kjshah@gmail.com>
2020-01-11 13:47:43 -0800

    new rollup file

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,80 @@
 import babel from 'rollup-plugin-babel';
 import filesize from 'rollup-plugin-filesize';
-import { terser } from "rollup-plugin-terser";
+import {
+    terser
+} from "rollup-plugin-terser";
 import resolve from "rollup-plugin-node-resolve"
 
-export default {
-    input: 'src/index.js',
-    output: {
-        name: 'Alpine',
-        file: 'dist/alpine.js',
-        format: 'umd',
-        sourcemap: true,
+
+export default [{
+        input: 'src/index.js',
+        output: {
+            name: 'Alpine',
+            file: 'dist/alpine.js',
+            format: 'umd',
+            sourcemap: true,
+        },
+        plugins: [
+            resolve(),
+            filesize(),
+            terser({
+                compress: {
+                    drop_debugger: false,
+                },
+            }),
+            babel({
+                exclude: 'node_modules/**',
+                presets: [
+                    [
+                        '@babel/preset-env',
+                        {
+                            targets: {
+                                node: 'current',
+                            },
+                        },
+                    ],
+                ],
+            })
+        ]
     },
-    plugins: [
-        resolve(),
-        filesize(),
-        terser({
-            compress: {
-                drop_debugger: false,
-            },
-        }),
-        babel({
-            exclude: 'node_modules/**'
-        })
-    ]
-}
+    {
+        input: 'src/index.js',
+        output: {
+            name: 'Alpine',
+            file: 'dist/alpine-ie11.js',
+            format: 'umd',
+            sourcemap: true,
+        },
+        plugins: [
+            resolve(),
+            filesize(),
+            terser({
+                compress: {
+                    drop_debugger: false,
+                },
+            }),
+            babel({
+                babelrc: false,
+                exclude: 'node_modules/**',
+                presets: [
+                    [
+                        "@babel/preset-env",
+                        {
+                            targets: {
+                                browsers: "> 0.5%, ie >= 11"
+                            },
+                            modules: false,
+                            spec: true,
+                            useBuiltIns: "usage",
+                            forceAllTransforms: true,
+                            corejs: {
+                                version: 3,
+                                proposals: false
+                            }
+                        }
+                    ]
+                ]
+            })
+        ]
+    }
+]

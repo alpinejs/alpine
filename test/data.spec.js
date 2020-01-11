7235bea632b093ea4c80e0476e2bab22a58e436e
Caleb Porzio <calebporzio@gmail.com>
2020-01-11 14:17:38 -0500

    Merge master

--- test/data.spec.js
+++ test/data.spec.js
@@ -52,10 +52,6 @@ test('functions in x-data are reactive', async () => {
     document.body.innerHTML = `
         <div x-data="{ foo: 'bar', getFoo() {return this.foo}}">
             <span x-text="getFoo()"></span>
-<<<<<<< HEAD
-
-=======
->>>>>>> master
             <button x-on:click="foo = 'baz'"></button>
         </div>
     `

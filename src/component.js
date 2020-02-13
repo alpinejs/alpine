d157b04fc1ce96694205b2a8fc8abb7eef3deb10
Caleb Porzio <calebporzio@gmail.com>
2020-02-13 10:19:45 -0500

    Make x-show wait for children to hide

--- src/component.js
+++ src/component.js
@@ -31,6 +31,9 @@ export default class Component {
             this.nextTickStack.push(callback)
         }
 
+        this.showDirectiveStack = []
+        this.showDirectiveLastElement
+
         var initReturnedCallback
         if (initExpression) {
             // We want to allow data manipulation, but not trigger DOM updates just yet.
@@ -131,6 +134,8 @@ export default class Component {
             el.__x = new Component(el)
         })
 
+        this.executeAndClearRemainingShowDirectiveStack()
+
         // Walk through the $nextTick stack and clear it as we go.
         while (this.nextTickStack.length > 0) {
             this.nextTickStack.shift()()
@@ -157,6 +162,34 @@ export default class Component {
         }, el => {
             el.__x = new Component(el)
         })
+
+        this.executeAndClearRemainingShowDirectiveStack()
+
+        // Walk through the $nextTick stack and clear it as we go.
+        while (this.nextTickStack.length > 0) {
+            this.nextTickStack.shift()()
+        }
+    }
+
+    executeAndClearRemainingShowDirectiveStack() {
+        // The goal here is to start all the x-show transitions
+        // and build a nested promise chain so that elements
+        // only hide when the children are finished hiding.
+        this.showDirectiveStack.reverse().map(thing => {
+            return new Promise(resolve => {
+                thing(finish => {
+                    resolve(finish)
+                })
+            })
+        }).reduce((nestedPromise, promise) => {
+            return nestedPromise.then(() => {
+                return promise.then(finish => finish())
+            })
+        }, Promise.resolve(() => {}))
+
+        // We've processed the handler stack. let's clear it.
+        this.showDirectiveStack = []
+        this.showDirectiveLastElement = undefined
     }
 
     updateElement(el, extraVars) {
@@ -211,7 +244,7 @@ export default class Component {
                 case 'show':
                     var output = this.evaluateReturnExpression(el, expression, extraVars)
 
-                    handleShowDirective(el, output, initialUpdate)
+                    handleShowDirective(this, el, output, modifiers, initialUpdate)
                     break;
 
                 case 'if':
--- src/directives/show.js
+++ src/directives/show.js
@@ -1,21 +1,53 @@
 import { transitionIn, transitionOut } from '../utils'
 
-export function handleShowDirective(el, value, initialUpdate = false) {
-    if (! value) {
-        if ( el.style.display !== 'none' ) {
-            transitionOut(el, () => {
+export function handleShowDirective(component, el, value, modifiers, initialUpdate = false) {
+    const handle = (resolve) => {
+        const hide = () => {
+            resolve(() => {
                 el.style.display = 'none'
-            }, initialUpdate)
+            })
         }
-    } else {
-        if ( el.style.display !== '' ) {
-            transitionIn(el, () => {
-                if (el.style.length === 1) {
-                    el.removeAttribute('style')
-                } else {
-                    el.style.removeProperty('display')
-                }
-            }, initialUpdate)
+
+        const show = () => {
+            resolve(() => {})
+            // Perform showing immediately.
+            if (el.style.length === 1) {
+                el.removeAttribute('style')
+            } else {
+                el.style.removeProperty('display')
+            }
+        }
+
+        if (! value) {
+            if ( el.style.display !== 'none' ) {
+                transitionOut(el, hide, initialUpdate)
+            }
+        } else {
+            if ( el.style.display !== '' ) {
+                transitionIn(el, show, initialUpdate)
+            }
         }
     }
+
+    // The working of x-show is a bit complex because we need to
+    // wait for any child transitions to finish before hiding
+    // some element. Also, this has to be done recursively.
+
+    // If x-show.immediate, foregoe the waiting.
+    if (modifiers.includes('immediate')) {
+        handle(finish => finish())
+        return
+    }
+
+    // x-show is encountered during a DOM tree walk. If an element
+    // we encounter is NOT a child of another x-show element we
+    // can execute the previous x-show stack (if one exists).
+    if (component.showDirectiveLastElement && ! component.showDirectiveLastElement.contains(el)) {
+        component.executeAndClearRemainingShowDirectiveCallbacks()
+    }
+
+    // We'll push the handler onto a stack to be handled later.
+    component.showDirectiveStack.push(handle)
+
+    component.showDirectiveLastElement = el
 }

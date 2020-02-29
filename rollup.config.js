46dd03c7616124224a909c56d3c9a027a61a4b81
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-02-29 13:57:52 +0000

    Add ESM output rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,30 +3,59 @@ import filesize from 'rollup-plugin-filesize';
 import { terser } from "rollup-plugin-terser";
 import resolve from "rollup-plugin-node-resolve"
 import stripCode from 'rollup-plugin-strip-code';
+import pkg from './package.json';
 
-export default {
-    input: 'src/index.js',
-    output: {
-        name: 'Alpine',
-        file: 'dist/alpine.js',
-        format: 'umd',
-        sourcemap: true,
+export default [
+    {
+        input: 'src/index.js',
+        output: {
+            name: 'Alpine',
+            file: pkg.main,
+            format: 'umd',
+            sourcemap: true,
+        },
+        plugins: [
+            resolve(),
+            filesize(),
+            terser({
+                mangle: false,
+                compress: {
+                    drop_debugger: false,
+                },
+            }),
+            babel({
+                exclude: 'node_modules/**'
+            }),
+            stripCode({
+                start_comment: 'IE11-ONLY:START',
+                end_comment: 'IE11-ONLY:END'
+            })
+        ]
     },
-    plugins: [
-        resolve(),
-        filesize(),
-        terser({
-            mangle: false,
-            compress: {
-                drop_debugger: false,
-            },
-        }),
-        babel({
-            exclude: 'node_modules/**'
-        }),
-        stripCode({
-            start_comment: 'IE11-ONLY:START',
-            end_comment: 'IE11-ONLY:END'
-        })
-    ]
-}
+    {
+        input: 'src/index.js',
+        output: {
+            name: 'Alpine ES Module',
+            file: pkg.module,
+            format: 'es',
+            sourcemap: true,
+        },
+        plugins: [
+            resolve(),
+            filesize(),
+            terser({
+                mangle: false,
+                compress: {
+                    drop_debugger: false,
+                },
+            }),
+            babel({
+                exclude: 'node_modules/**'
+            }),
+            stripCode({
+                start_comment: 'IE11-ONLY:START',
+                end_comment: 'IE11-ONLY:END'
+            })
+        ]
+    }
+]

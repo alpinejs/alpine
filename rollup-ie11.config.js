cb431c982721417feb847c119d56cdb56d8aae9c
Caleb Porzio <calebporzio@gmail.com>
2020-03-07 00:33:05 -0500

    Remove terser and re-build

--- rollup-ie11.config.js
+++ rollup-ie11.config.js
@@ -1,6 +1,5 @@
 import babel from 'rollup-plugin-babel';
 import filesize from 'rollup-plugin-filesize';
-import { terser } from 'rollup-plugin-terser';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import multi from '@rollup/plugin-multi-entry';
@@ -12,7 +11,6 @@ export default {
         name: 'Alpine',
         file: 'dist/alpine-ie11.js',
         format: 'umd',
-        sourcemap: true,
     },
     plugins: [
         multi(),
@@ -21,12 +19,6 @@ export default {
         replace({ "process.env.NODE_ENV": "'production'" }),
         resolve(),
         filesize(),
-        terser({
-            mangle: false,
-            compress: {
-                drop_debugger: false,
-            }
-        }),
         babel({
             babelrc: false,
             exclude: 'node_modules/**',
--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import babel from 'rollup-plugin-babel';
 import filesize from 'rollup-plugin-filesize';
-import { terser } from 'rollup-plugin-terser';
 import resolve from 'rollup-plugin-node-resolve';
 import stripCode from 'rollup-plugin-strip-code';
 import replace from '@rollup/plugin-replace';
@@ -11,19 +10,12 @@ export default {
         name: 'Alpine',
         file: 'dist/alpine.js',
         format: 'umd',
-        sourcemap: true,
     },
     plugins: [
         // 'observable-membrane' uses process.env. We don't have that...
         replace({ "process.env.NODE_ENV": "'production'" }),
         resolve(),
         filesize(),
-        terser({
-            mangle: false,
-            compress: {
-                drop_debugger: false,
-            }
-        }),
         babel({
             exclude: 'node_modules/**'
         }),

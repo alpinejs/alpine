e80cefde650c2d8703c31274313a3f0d962e5476
Caleb Porzio <calebporzio@gmail.com>
2020-03-07 00:52:29 -0500

    Adjust tests

--- test/constructor.spec.js
+++ test/constructor.spec.js
@@ -279,3 +279,19 @@ test('can clone an existing component to a new element', async () => {
 
     expect(document.querySelector('span').innerText).toEqual('bar')
 })
+
+test('x-attributes are matched exactly', async () => {
+    document.body.innerHTML = `
+        <div x-data="{ showElement: false }">
+            <div id="el1" x-show="showElement" />
+            <div id="el2" xxx-show="showElement" />
+            <div id="el3" x-showabc="showElement" />
+        </div>
+    `
+
+    Alpine.start()
+
+    expect(document.getElementById('el1').style.display).toEqual('none')
+    expect(document.getElementById('el2').style.display).not.toEqual('none')
+    await wait(() => { expect(document.getElementById('el3').style.display).not.toEqual('none') })
+})

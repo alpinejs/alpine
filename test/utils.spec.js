71e3ed7756e880df59e1e0ce5d82171ccb6745d3
Julio Camargo <julioocamargoo@gmail.com>
2020-04-03 13:44:10 -0300

    adjusting utils test

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,7 +1,7 @@
-import utils from '../src/utils'
+import { arrayUnique } from '../src/utils'
 
 test('utils/arrayUnique', () => {
     const arrayMock = [1, 1, 2, 3, 1, 'a', 'b', 'c', 'b']
-    const expected = utils.arrayUnique(arrayMock)
+    const expected = arrayUnique(arrayMock)
     expect(expected).toEqual([1, 2, 3, 'a', 'b', 'c'])
 })

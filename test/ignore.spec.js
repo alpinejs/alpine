74b821d8c3c78f5a1c1d50e40f173fdc805f6156
Muzaffer Dede <muzaffer@nemrut.co>
2020-04-22 21:27:59 +0800

    more imporovements on tests and docs

--- test/ignore.spec.js
+++ test/ignore.spec.js
@@ -19,7 +19,7 @@ test('x-ignore ignores initialization of any new components within the node', as
     await wait(() => { expect(document.querySelector('span').innerText).toEqual(undefined) })
 })
 
-test('x-ignore if set to false it will initialize any new components within the node', async () => {
+test('if x-ignore set to false it will initialize any new components within the node', async () => {
     document.body.innerHTML = `
     <div x-ignore="false">
         <div x-data="{foo:'foo'}">
@@ -30,10 +30,34 @@ test('x-ignore if set to false it will initialize any new components within the 
 
     Alpine.start()
 
-    await wait(() => { expect(document.querySelector('span').innerText).toEqual('foo') })
+    await wait(() => {
+        expect(document.querySelector('span').innerText).toEqual('foo')
+     })
 })
 
-test('x-ignore if empty ignores initialization of any new components within the node', async () => {
+test('Nested x-ignore set to true nodes will not initialize any new components within the node', async () => {
+    document.body.innerHTML = `
+    <div x-ignore="false">
+        <div x-data="{foo:'foo'}">
+            <span x-text="foo"></span>
+        </div>
+        <div x-ignore="true">
+            <div x-data="{bar:'baz'}">
+                <h1 x-text="bar"></h1>
+            </div>
+        </div>
+    </div>
+    `
+
+    Alpine.start()
+
+    await wait(() => {
+        expect(document.querySelector('span').innerText).toEqual('foo')
+        expect(document.querySelector('h1').innerText).toEqual(undefined)
+     })
+})
+
+test('if x-ignore attribute without a value, it will default to "true"', async () => {
     document.body.innerHTML = `
     <div x-ignore>
         <div x-data="{foo:'foo'}">

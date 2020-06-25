e560749c68f18688b181534b14d6a0c3a18cbd8a
Parviz/Ibragim Bekliev <nightkon95@gmail.com>
2020-06-25 16:14:16 +0300

    Avoid unnecessary loops

--- src/utils.js
+++ src/utils.js
@@ -109,13 +109,11 @@ export function getXAttrs(el, component, type) {
         // Add x-spread directives to the pile of existing directives.
         directives = directives.concat(Object.entries(spreadObject).map(([name, value]) => parseHtmlAttribute({ name, value })))
     }
+    
+    // If no type is passed in for filtering, bypass filter
+    if (!type) return directives;
 
-    return directives.filter(i => {
-        // If no type is passed in for filtering, bypass filter
-        if (! type) return true
-
-        return i.type === type
-    })
+    return directives.filter(i => i.type === type)
 }
 
 function parseHtmlAttribute({ name, value }) {

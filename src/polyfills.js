bbae6b9686acc5ce4b73b8f88856a5455bcc1d5c
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-06-29 19:08:51 +0100

    switch to 'events-polyfill' package, superseded custom-events-polyfill

--- src/polyfills.js
+++ src/polyfills.js
@@ -5,6 +5,7 @@ import "element-closest/browser.js"
 import "element-remove"
 import "classlist-polyfill"
 import "@webcomponents/template"
-import "custom-event-polyfill"
+import "events-polyfill/src/constructors/CustomEvent"
+import "events-polyfill/src/ListenerOptions"
 
 SVGElement.prototype.contains = SVGElement.prototype.contains || HTMLElement.prototype.contains

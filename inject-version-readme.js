e37bf2ec368f3c9df129603eb04fa0edb1f46a83
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-08-28 18:13:15 +0100

    fix japanese README (IE11 + pinned version)

--- inject-version-readme.js
+++ inject-version-readme.js
@@ -5,6 +5,6 @@ const pkg = require('./package.json');
 const readmeTranslations = fs.readdirSync('.').filter((name) => name.includes('README'))
 readmeTranslations.forEach((readmeName) => {
     const original = fs.readFileSync(readmeName, 'utf8')
-    const updated = original.replace(/[0-9]\.[0-9]\.[0-9]/gi, pkg.version)
+    const updated = original.replace(/[0-9]+\.[0-9]+\.[0-9]+/gi, pkg.version)
     fs.writeFileSync(readmeName, updated, 'utf8')
 })

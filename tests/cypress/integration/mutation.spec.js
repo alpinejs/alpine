3825a136592987c2321d5d51926d046579e94a19
Caleb Porzio <calebporzio@gmail.com>
2021-06-19 23:40:07 -0400

    Cleanup nested removed element's attributes

--- tests/cypress/integration/mutation.spec.js
+++ tests/cypress/integration/mutation.spec.js
@@ -25,6 +25,33 @@ test('element side effects are cleaned up after the elements are removed',
     }
 )
 
+test('nested element side effects are cleaned up after the parent is removed',
+    html`
+        <div x-data="{ foo: 1, bar: 1 }">
+            <button @click="bar++">bar</button>
+            <a href="#" @click.prevent="$refs.article.remove()">remove</a>
+
+            <article x-ref="article">
+                <span x-text="(() => { foo = foo + 1; return bar })"></span>
+            </article>
+
+            <h1 x-text="foo"></h1>
+            <h2 x-text="bar"></h2>
+        </div>
+    `,
+    ({ get }) => {
+        get('h1').should(haveText('2'))
+        get('h2').should(haveText('1'))
+        get('button').click()
+        get('h1').should(haveText('3'))
+        get('h2').should(haveText('2'))
+        get('a').click()
+        get('button').click()
+        get('h1').should(haveText('3'))
+        get('h2').should(haveText('3'))
+    }
+)
+
 test('can mutate directive value',
     html`
         <div x-data="{ foo: 'bar', bar: 'baz' }">

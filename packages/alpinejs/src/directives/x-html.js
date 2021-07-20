b44cb519c9f1e879b1a11d735212534f07440622
Tibor Barta <tiborbarta87@gmail.com>
2021-07-20 21:33:13 +0200

    code formatting

--- packages/alpinejs/src/directives/x-html.js
+++ packages/alpinejs/src/directives/x-html.js
@@ -1,7 +1,7 @@
 import {directive} from '../directives'
-import {nextTick} from "../nextTick";
+import {nextTick} from "../nextTick"
 
-directive('html', (el, {expression}, {effect, evaluateLater}) => {
+directive('html', (el, { expression }, { effect, evaluateLater }) => {
     let evaluate = evaluateLater(expression)
 
     effect(() => {

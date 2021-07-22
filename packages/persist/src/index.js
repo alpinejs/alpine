4cb11de2154c8dff791707cda1a939d19d49bad4
Caleb Porzio <calebporzio@gmail.com>
2021-07-22 11:17:13 -0400

    Finalize and document $persist (#1828)

--- packages/persist/src/index.js
+++ packages/persist/src/index.js
@@ -1,21 +1,40 @@
 
 export default function (Alpine) {
+    let alias
+
     Alpine.magic('persist', (el, { interceptor }) => {
         return interceptor((initialValue, getter, setter, path, key) => {
-            let initial = localStorage.getItem(path)
-                ? localStorage.getItem(path)
+            let lookup = alias || `_x_${path}`
+
+            let initial = storageHas(lookup)
+                ? storageGet(lookup)
                 : initialValue
 
             setter(initialValue)
 
             Alpine.effect(() => {
                 let value = getter()
-                localStorage.setItem(path, value)
+
+                storageSet(lookup, value)
 
                 setter(value)
             })
 
             return initial
+        }, func => {
+            func.as = key => { alias = key; return func }
         })
     })
+}
+
+function storageHas(key) {
+    return localStorage.getItem(key) !== null
+}
+
+function storageGet(key) {
+    return JSON.parse(localStorage.getItem(key))
+}
+
+function storageSet(key, value) {
+    localStorage.setItem(key, JSON.stringify(value))
 }
--- tests/cypress/integration/plugins/persist.spec.js
+++ tests/cypress/integration/plugins/persist.spec.js
@@ -1,6 +1,6 @@
 import { haveText, html, test } from '../../utils'
 
-test('can perist data',
+test('can perist number',
     [html`
         <div x-data="{ count: $persist(1) }">
             <button @click="count++">Inc</button>
@@ -13,5 +13,39 @@ test('can perist data',
         get('span').should(haveText('2'))
         reload()
         get('span').should(haveText('2'))
+    },
+)
+
+test('can perist string',
+    [html`
+        <div x-data="{ message: $persist('foo') }">
+            <input x-model="message">
+
+            <span x-text="message"></span>
+        </div>
+    `],
+    ({ get }, reload) => {
+        get('span').should(haveText('foo'))
+        get('input').clear().type('bar')
+        get('span').should(haveText('bar'))
+        reload()
+        get('span').should(haveText('bar'))
+    },
+)
+
+test('can perist array',
+    [html`
+        <div x-data="{ things: $persist(['foo', 'bar']) }">
+            <button @click="things.push('baz')"></button>
+
+            <span x-text="things.join('-')"></span>
+        </div>
+    `],
+    ({ get }, reload) => {
+        get('span').should(haveText('foo-bar'))
+        get('button').click()
+        get('span').should(haveText('foo-bar-baz'))
+        reload()
+        get('span').should(haveText('foo-bar-baz'))
     },
 )

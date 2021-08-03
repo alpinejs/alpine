40646142d0f570745c17a80d5da1c633279fbfac
Dylan Anderson <dylana@ualberta.ca>
2021-08-03 15:24:08 -0600

    Revert "Add .dot modifier for "." separated events"
    This reverts commit 9eb457bb17f4befa11569e0f49ca0d1033444746.

--- packages/alpinejs/src/utils/on.js
+++ packages/alpinejs/src/utils/on.js
@@ -10,7 +10,6 @@ export default function on (el, event, modifiers, callback) {
     // handler more flexibly in a "middleware" style.
     let wrapHandler = (callback, wrapper) => (e) => wrapper(callback, e)
 
-    if (modifiers.includes("dot")) event = dotSyntax(event);
     if (modifiers.includes('camel')) event = camelCase(event)
     if (modifiers.includes('passive')) options.passive = true
     if (modifiers.includes('window')) listenerTarget = window
@@ -71,10 +70,6 @@ export default function on (el, event, modifiers, callback) {
     }
 }
 
-function dotSyntax(subject) {
-    return subject.replace(/-/g, ".");
-}
-  
 function camelCase(subject) {
     return subject.toLowerCase().replace(/-(\w)/g, (match, char) => char.toUpperCase())
 }

28d7484c8f2944127243528df210a492cb30de28
dand <daniel.dent@borderconnect.com>
2021-08-04 15:15:28 -0400

    add back error for better dev experience

--- packages/alpinejs/src/utils/error.js
+++ packages/alpinejs/src/utils/error.js
@@ -9,7 +9,11 @@ export function tryCatch(el, expression, callback, ...args) {
 }
 
 export function handleError(error, el, expression = undefined) {
+    Object.assign( error, { el, expression } )
+
     console.warn(`Alpine Expression Error: ${error.message}\n\n${ expression ? 'Expression: \"' + expression + '\"\n\n' : '' }`, el)
 
     dispatch( document, "alpine:error", { error, el, expression } )
+
+    setTimeout( () => { throw error }, 0 )
 }

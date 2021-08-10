50632341a1c11f705a7b4aa439b64f6f76a7c6bb
Muzaffer Dede <dedemuzaffer@gmail.com>
2021-08-10 12:44:45 +0800

    Added tests

--- tests/cypress/integration/plugins/intersect.spec.js
+++ tests/cypress/integration/plugins/intersect.spec.js
@@ -2,7 +2,7 @@ import { haveText, test, html } from '../../utils'
 
 test('can intersect',
     [html`
-    <div x-data="{ count: 0 }" x-init="setTimeout(() => count++, 300)">
+    <div x-data="{ count: 0 }">
         <span x-text="count"></span>
 
         <div x-intersect="count++" style="margin-top: 100vh;" id="1">hi</div>
@@ -10,13 +10,52 @@ test('can intersect',
     `],
     ({ get }, reload) => {
         get('span').should(haveText('0'))
+        get('#1').scrollIntoView({duration: 100})
         get('span').should(haveText('1'))
-        get('#1').scrollIntoView()
+        get('span').scrollIntoView({duration: 100})
         get('span').should(haveText('1'))
-        get('span').scrollIntoView()
-        get('span').should(haveText('1'))
-        get('#1').scrollIntoView()
+        get('#1').scrollIntoView({duration: 100})
         get('span').should(haveText('2'))
+    },
+)
+
+test('It should emit "enter" event when the component is intersected',
+    [html`
+    <div x-data="{ count: 0 }">
+        <span x-text="count"></span>
+
+        <div x-intersect @enter="count++" style="margin-top: 100vh;" id="1">hi</div>
+    </div>
+    `],
+    ({ get }, reload) => {
+        get('span').should(haveText('0'))
+        get('#1').scrollIntoView({duration: 100})
+        get('span').should(haveText('1'))
+        get('span').scrollIntoView({duration: 100})
+        get('span').should(haveText('1'))
+        get('#1').scrollIntoView({duration: 100})
+        get('span').should(haveText('2'))
+    },
+)
+
+test('It should emit "leave" event when the component is intersected',
+    [html`
+    <div x-data="{ count: 0 }">
+        <span x-text="count"></span>
+
+        <div x-intersect @leave="count++" style="margin-top: 100vh;" id="1">hi</div>
+    </div>
+    `],
+    ({ get }, reload) => {
+        get('span').should(haveText('1'))
+        get('#1').scrollIntoView({duration: 100})
+        get('span').should(haveText('1'))
+        get('span').scrollIntoView({duration: 100})
+        get('span').should(haveText('2'))
+        get('#1').scrollIntoView({duration: 100})
+        get('span').should(haveText('2'))
+        get('span').scrollIntoView({duration: 100})
+        get('span').should(haveText('3'))
     },
 )
 
@@ -30,12 +69,11 @@ test('.once',
     `],
     ({ get }, reload) => {
         get('span').should(haveText('0'))
+        get('#1').scrollIntoView({duration: 100})
         get('span').should(haveText('1'))
-        get('#1').scrollIntoView()
+        get('span').scrollIntoView({duration: 100})
         get('span').should(haveText('1'))
-        get('span').scrollIntoView()
-        get('span').should(haveText('1'))
-        get('#1').scrollIntoView()
-        get('span').should(haveText('1'))
+        get('#1').scrollIntoView({duration: 100})
+        get('span').should(haveText('2'))
     },
 )

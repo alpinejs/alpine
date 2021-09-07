d256db26e716ceed222efe3b1d25d8a86107494c
dand <daniel.dent@borderconnect.com>
2021-09-07 10:58:12 -0400

    remove error event dispatching

--- packages/alpinejs/src/utils/error.js
+++ packages/alpinejs/src/utils/error.js
@@ -1,5 +1,3 @@
-import {dispatch} from "./dispatch";
-
 export function tryCatch(el, expression, callback, ...args) {
     try {
         return callback(...args)
@@ -12,8 +10,6 @@ export function handleError(error, el, expression = undefined) {
     Object.assign( error, { el, expression } )
 
     console.warn(`Alpine Expression Error: ${error.message}\n\n${ expression ? 'Expression: \"' + expression + '\"\n\n' : '' }`, el)
-
-    dispatch( document, "alpine:error", { error, el, expression } )
 
     setTimeout( () => { throw error }, 0 )
 }

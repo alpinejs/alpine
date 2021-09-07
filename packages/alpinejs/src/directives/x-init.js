alpinejs/alpine

commit 74c7ba4d551a26bd7951eb6824975e4b0ea57100

Fix empty x-init (#1993)
* Update x-init.js

* added test

packages/alpinejs/src/directives/x-init.js
@@ -5,4 +5,4 @@ import { evaluate } from "../evaluator";
 
 addInitSelector(() => `[${prefix('init')}]`)
 
-directive('init', skipDuringClone((el, { expression }) => evaluate(el, expression, {}, false)))
+directive('init', skipDuringClone((el, { expression }) => !! expression.trim() && evaluate(el, expression, {}, false)))

tests/cypress/integration/directives/x-init.spec.js
@@ -35,3 +35,12 @@ test('can make deferred changes with $nextTick',
     `,
     ({ get }) => get('span').should(haveText('yo'))
 )
+
+test('x-init will not evaluate expression if it is empty',
+    html`
+        <div x-data="{ foo: 'bar' }" x-init=" ">
+            <span x-text="foo" x-ref="foo">baz</span>
+        </div>
+    `,
+    ({ get }) => get('span').should(haveText('bar'))
+)

commit 38ce2762790c0350bae403f112e0b8de602a4c5c

refactor: fix `x-init` bug
### Bug caused by #1993

The change breaks `x-init` expressions that are defined via `x-bind` and possibly other
directives that pass an object through. It assumes the value of `x-init` will always be a string
(empty or not) by using the `trim` function.

The following still works after this change:

```
<div x-init="$nextTick(() => { console.log('this works') })"></div>
```

But the following will now break:

```
<script>
const test = {
  ['x-init']() {
    return this.$nextTick(() => console.log('this will not be logged'));
  }
};
</script>

<div x-bind="test"></div>
```

### Change introduced to fix

Check the `expression` is a string before checking `trim` produces an empty string.

packages/alpinejs/src/directives/x-init.js
@@ -5,4 +5,12 @@ import { evaluate } from "../evaluator";
 
 addInitSelector(() => `[${prefix('init')}]`)
 
-directive('init', skipDuringClone((el, { expression }) => !! expression.trim() && evaluate(el, expression, {}, false)))
+directive('init', skipDuringClone((el, { expression }) => {
+  const evaluation = evaluate(el, expression, {}, false)
+
+  if (typeof expression === 'string') {
+    return !! expression.trim() && evaluation
+  }
+
+  return evaluation
+}))
0331078cc5df149c278703fe6a1dcc93344d2e02
Caleb Porzio <calebporzio@gmail.com>
2021-09-16 11:41:00 -0400

    Allow access to parent scope refs (#2077)

--- packages/alpinejs/src/magics/$refs.js
+++ packages/alpinejs/src/magics/$refs.js
@@ -1,4 +1,25 @@
 import { closestRoot } from '../lifecycle'
+import { mergeProxies } from '../scope'
 import { magic } from '../magics'
 
-magic('refs', el => closestRoot(el)._x_refs || {})
+magic('refs', el => {
+    if (el._x_refs_proxy) return el._x_refs_proxy
+
+    el._x_refs_proxy = mergeProxies(getArrayOfRefObject(el))
+
+    return el._x_refs_proxy
+})
+
+function getArrayOfRefObject(el) {
+    let refObjects = []
+
+    let currentEl = el
+
+    while (currentEl) {
+        if (currentEl._x_refs) refObjects.push(currentEl._x_refs)
+
+        currentEl = currentEl.parentNode
+    }
+
+    return refObjects
+}

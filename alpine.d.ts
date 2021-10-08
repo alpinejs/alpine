85dd164d9ce5fcab2aa44a3f42073e6f5f971b56
Mattéo Turini <69425093+matteoturini@users.noreply.github.com>
2021-10-08 10:05:53 -0300

    Create alpine.d.ts

---
+++ alpine.d.ts
@@ -0,0 +1,6 @@
+declare module Alpine {
+  function store<T = any>(name: string, value: undefined): T;
+  function store<T = any>(name: string, value: T): void;
+
+  function data<T = any>(name: string, callback: () => T): void;
+}

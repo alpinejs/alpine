be9a12c2f30c3de591bb4633f1504a61e0031de7
Simone Todaro <simo.todaro@gmail.com>
2021-10-11 16:01:38 +0100

    x-collapse not working properly when used with click.away (#2201)
    * Add failing test
    
    * Make x-collapse compatible with @click.away
    
    * Extend fix to elements with borders

--- tests/cypress/integration/plugins/collapse.spec.js
+++ tests/cypress/integration/plugins/collapse.spec.js
@@ -15,3 +15,34 @@ test('can collapse and expand element',
         get('h1').should(haveComputedStyle('height', '0px'))
     },
 )
+
+test('@click.away with x-collapse (prevent race condition)',
+    html`
+        <div x-data="{ show: false }">
+            <button @click="show = true">Show</button>
+
+            <h1 x-show="show" @click.away="show = false" x-collapse>h1</h1>
+        </div>
+    `,
+    ({ get }) => {
+        get('h1').should(haveComputedStyle('height', '0px'))
+        get('button').click()
+        get('h1').should(haveAttribute('style', 'overflow: hidden; height: auto;'))
+    }
+)
+
+
+test('@click.away with x-collapse and borders (prevent race condition)',
+    html`
+        <div x-data="{ show: false }">
+            <button @click="show = true">Show</button>
+
+            <h1 style="border: 1x solid" x-show="show" @click.away="show = false" x-collapse>h1</h1>
+        </div>
+    `,
+    ({ get }) => {
+        get('h1').should(haveComputedStyle('height', '0px'))
+        get('button').click()
+        get('h1').should(haveAttribute('style', 'overflow: hidden; height: auto;'))
+    }
+)

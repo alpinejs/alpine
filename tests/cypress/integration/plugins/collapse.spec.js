d50323c3d1b8a5aea50eb1e5c8e1f49aac960ae4
George Boot <884482+georgeboot@users.noreply.github.com>
2021-10-28 12:25:24 +0200

    Fix test

--- tests/cypress/integration/plugins/collapse.spec.js
+++ tests/cypress/integration/plugins/collapse.spec.js
@@ -28,7 +28,7 @@ test('@click.away with x-collapse (prevent race condition)',
     ({ get }) => {
         get('h1').should(haveComputedStyle('height', '0px'))
         get('button').click()
-        get('h1').should(haveAttribute('style', 'overflow: hidden; height: auto;'))
+        get('h1').should(haveAttribute('style', 'height: 0px; overflow: hidden;'))
     }
 )
 
@@ -44,6 +44,6 @@ test('@click.away with x-collapse and borders (prevent race condition)',
     ({ get }) => {
         get('h1').should(haveComputedStyle('height', '0px'))
         get('button').click()
-        get('h1').should(haveAttribute('style', 'overflow: hidden; height: auto;'))
+        get('h1').should(haveAttribute('style', 'height: 0px; overflow: hidden;'))
     }
 )

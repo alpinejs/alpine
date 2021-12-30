43a49ae5ae40670d94240ee1d0c74fe344312e9e
Marek Algoud <marek.algoud@gmail.com>
2021-12-30 14:58:03 +0100

    [FIX] add missing export

--- packages/resize/src/index.js
+++ packages/resize/src/index.js
@@ -2,22 +2,24 @@ function isNumeric(subject) {
     return ! Array.isArray(subject) && ! isNaN(subject)
 }
 
-Alpine.directive('resize', (el, { modifiers, expression }, { evaluateLater, cleanup }) => {
+export default function (Alpine) {
+    Alpine.directive('resize', (el, { modifiers, expression }, { evaluateLater, cleanup }) => {
 
-    let evaluate = evaluateLater(expression)
+        let evaluate = evaluateLater(expression)
 
-    if (modifiers.includes('debounce')) {
-        let nextModifier = modifiers[modifiers.indexOf('debounce')+1] || 'invalid-wait'
-        let wait = isNumeric(nextModifier.split('ms')[0]) ? Number(nextModifier.split('ms')[0]) : 250
+        if (modifiers.includes('debounce')) {
+            let nextModifier = modifiers[modifiers.indexOf('debounce')+1] || 'invalid-wait'
+            let wait = isNumeric(nextModifier.split('ms')[0]) ? Number(nextModifier.split('ms')[0]) : 250
 
-        evaluate = Alpine.debounce(evaluate, wait)
-    }
+            evaluate = Alpine.debounce(evaluate, wait)
+        }
 
-    const observer = new ResizeObserver(entries => {
-        entries.forEach(() => evaluate())
+        const observer = new ResizeObserver(entries => {
+            entries.forEach(() => evaluate())
+        })
+
+        observer.observe(el)
+
+        cleanup(() => observer.disconnect())
     })
-
-    observer.observe(el)
-
-    cleanup(() => observer.disconnect())
-})
+}

2d1669f248c926712971100e06ce8c32f1dc71db
Caleb Porzio <calebporzio@gmail.com>
2022-03-21 10:51:39 -0400

    Cleanup imports

--- packages/alpinejs/src/directives/x-html.js
+++ packages/alpinejs/src/directives/x-html.js
@@ -1,6 +1,4 @@
-import { evaluateLater } from '../evaluator'
 import { directive } from '../directives'
-import { mutateDom } from '../mutation'
 
 directive('html', (el, { expression }, { effect, evaluateLater }) => {
     let evaluate = evaluateLater(expression)

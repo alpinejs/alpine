d8b3499ee4da0eeeb55197f4d104397ec84a867c
Alexey Khachaturyan <53150670+devcustrom@users.noreply.github.com>
2022-04-19 16:19:30 +0300

    Fixed link in error message (#2836)

--- packages/alpinejs/src/directives/index.js
+++ packages/alpinejs/src/directives/index.js
@@ -27,5 +27,5 @@ warnMissingPluginDirective('Focus', 'trap', 'focus')
 warnMissingPluginDirective('Mask', 'mask', 'mask')
 
 function warnMissingPluginDirective(name, directiveName, slug) {
-    directive(directiveName, (el) => warn(`You can't use [x-${directiveName}] without first installing the "${name}" plugin here: https://alpine.dev/plugins/${slug}`, el))
+    directive(directiveName, (el) => warn(`You can't use [x-${directiveName}] without first installing the "${name}" plugin here: https://alpinejs.dev/plugins/${slug}`, el))
 }

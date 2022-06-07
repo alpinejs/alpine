d921c010bb979a0d0c42ec0869eaf183f76475b4
Stepan Jakl <stevie.jakl@gmail.com>
2022-06-07 10:36:31 +0100

    allow string falsy value

--- packages/mask/src/index.js
+++ packages/mask/src/index.js
@@ -49,7 +49,7 @@ export default function (Alpine) {
             let template = templateFn(input)
 
             // If a template value is `falsy`, then don't process the input value
-            if(!template) return false
+            if(!template || template === 'false') return false
 
             // If they hit backspace, don't process input.
             if (lastInputValue.length - el.value.length === 1) {

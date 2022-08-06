c1454593920642b353120b3afcb12fc592090aa8
Daniel Puckowski <puckowski.d@gmail.com>
2022-08-06 10:18:14 -0400

    Use structuredClone for improved watch
    * Use strucutredClone for improved watch to avoid object by reference
      issues.

--- packages/alpinejs/src/magics/$watch.js
+++ packages/alpinejs/src/magics/$watch.js
@@ -7,20 +7,48 @@ magic('watch', (el, { evaluateLater, effect }) => (key, callback) => {
 
     let oldValue
 
+    const cloneArray = (arr) => {
+        const clonedArr = [];
+
+        for (let i = 0; i < arr.length; ++i) {
+            if (Array.isArray(arr[i])) {
+                clonedArr.push(cloneArray(arr[i]));
+            } else if (arr[i] === Object(arr[i])) {
+                clonedArr.push(structuredClone({ ...arr[i] }));
+            } else {
+                clonedArr.push(arr[i]);
+            }
+        }
+
+        return clonedArr;
+    };
+
     let effectReference = effect(() => evaluate(value => {
         // JSON.stringify touches every single property at any level enabling deep watching
         JSON.stringify(value)
 
-        if (! firstTime) {
+        if (!firstTime) {
             // We have to queue this watcher as a microtask so that
             // the watcher doesn't pick up its own dependencies.
             queueMicrotask(() => {
                 callback(value, oldValue)
 
-                oldValue = JSON.parse(JSON.stringify(value))
+                if (Array.isArray(value)) {
+                    oldValue = cloneArray(value);
+                } else if (value === Object(value)) {
+                    oldValue = structuredClone({ ...value });
+                } else {
+                    oldValue = value
+                }
             })
         } else {
-            oldValue = JSON.parse(JSON.stringify(value))
+            if (Array.isArray(value)) {
+                oldValue = cloneArray(value);
+            } else if (value === Object(value)) {
+                oldValue = structuredClone({ ...value });
+            } else {
+                oldValue = value
+            }
         }
 
         firstTime = false

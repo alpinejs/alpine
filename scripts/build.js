9bb5a50bb4bf3929090976859ec2dd2aed3a47e5
Neil Carlo Sucuangco <succute@yahoo.com>
2022-10-29 16:35:13 +0800

    remove unnecessary space

--- scripts/build.js
+++ scripts/build.js
@@ -115,4 +115,4 @@ function bytesToSize(bytes) {
     const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10)
     if (i === 0) return `${bytes} ${sizes[i]}`
     return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`
-  }
+}

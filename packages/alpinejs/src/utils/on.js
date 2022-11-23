df21e2a46ebeec2f6a7ca26fb5245730883fb615
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-11-24 08:06:14 +0900

    :bug: prevents coercion of space and underscore to minus (#3134)
    * :test_tube: Adds test for checking key discernment
    
    Expanding the keydown modifiers test introduced some issues regarding ensuring the these options were all treated separately without overlap.
    
    * :white_check_mark: :bug: Fixes coercion of space and underscore
    
    * :construction: Completes Test
    
    Co-authored-by: Marc Reichel <mail@marcreichel.de>
    
    Co-authored-by: Marc Reichel <mail@marcreichel.de>

--- packages/alpinejs/src/utils/on.js
+++ packages/alpinejs/src/utils/on.js
@@ -93,6 +93,8 @@ function isNumeric(subject){
 }
 
 function kebabCase(subject) {
+    if ([' ','_'].includes(subject
+    )) return subject
     return subject.replace(/([a-z])([A-Z])/g, '$1-$2').replace(/[_\s]/, '-').toLowerCase()
 }
 
@@ -154,8 +156,8 @@ function keyToModifiers(key) {
     let modifierToKeyMap = {
         'ctrl': 'control',
         'slash': '/',
-        'space': '-',
-        'spacebar': '-',
+        'space': ' ',
+        'spacebar': ' ',
         'cmd': 'meta',
         'esc': 'escape',
         'up': 'arrow-up',
@@ -164,6 +166,8 @@ function keyToModifiers(key) {
         'right': 'arrow-right',
         'period': '.',
         'equal': '=',
+        'minus': '-',
+        'underscore': '_',
     }
 
     modifierToKeyMap[key] = key

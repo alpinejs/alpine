2b03136ce4eebc5339cab76ebc1690307194ecee
Ferran Conde Codorniu <ferran.conde.codorniu@cosanta.nl>
2022-12-02 17:02:26 +0100

    Revert "Ensure the `flushing` flag in `scheduler.js` is properly reset even if a job callback executes a faulty implementation where an uncaught exception is raised."
    This reverts commit 32130fe38d79b0c5fe0e22df35da64b4bd55d183.

--- packages/alpinejs/src/scheduler.js
+++ packages/alpinejs/src/scheduler.js
@@ -28,13 +28,11 @@ export function flushJobs() {
     flushPending = false
     flushing = true
 
-    try {
-        for (let i = 0; i < queue.length; i++) {
-            queue[i]()
-        }
-    } finally {
-        queue.length = 0
-
-        flushing = false
+    for (let i = 0; i < queue.length; i++) {
+        queue[i]()
     }
+
+    queue.length = 0
+
+    flushing = false
 }

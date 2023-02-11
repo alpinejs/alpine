c1db0f1725237015759d87988c72909784012d79
Restartz <restart@live.nl>
2023-02-11 13:45:09 +0100

    Update mask.spec.js

--- tests/cypress/integration/plugins/mask.spec.js
+++ tests/cypress/integration/plugins/mask.spec.js
@@ -60,6 +60,17 @@ test('x-mask with x-model',
     },
 )
 
+test('x-mask with x-model initial value',
+    [html`
+        <div x-data="{ value: '1234567890' }">
+            <input id="1" x-mask="(999) 999-9999" x-model="value">
+        </div>
+    `],
+    ({ get }) => {
+        get('#1').should(haveValue('(123) 456-7890'))
+    },
+)
+
 test('x-mask with a falsy input',
     [html`<input x-data x-mask="">`],
     ({ get }) => {

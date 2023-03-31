f494c5e432136812e134ea3c6c57a36214d9fb2c
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-03-31 18:04:06 +0400

    :sparkles: Allows more Plugin shapes

--- packages/alpinejs/src/plugin.js
+++ packages/alpinejs/src/plugin.js
@@ -1,5 +1,7 @@
 import Alpine from './alpine'
 
-export function plugin(callback) {
-    callback(Alpine)
+export function plugin(callback, ...callbacks) {
+    if (callbacks.length) plugin(callbacks)
+    if (Array.isArray(callback)) callback.forEach(cb => cb(Alpine));
+    else callback(Alpine)
 }

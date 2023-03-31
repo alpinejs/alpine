2585eb42443339bc786acaece04dcd075c622e0b
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-03-31 18:34:24 +0400

    :recycle: Refactors for speed
    and power

--- packages/alpinejs/src/plugin.js
+++ packages/alpinejs/src/plugin.js
@@ -1,7 +1,5 @@
-import Alpine from './alpine'
+import Alpine from "./alpine";
 
-export function plugin(callback, ...callbacks) {
-    if (callbacks.length) plugin(callbacks)
-    if (Array.isArray(callback)) callback.forEach(cb => cb(Alpine));
-    else callback(Alpine)
+export function plugin(...callbacks) {
+    callbacks.flat(Infinity).forEach((callback) => callback(Alpine));
 }

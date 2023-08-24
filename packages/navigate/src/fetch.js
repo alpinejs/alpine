42cbf6c2fa9453cae39e0c8aeede8f6043692983
Caleb Porzio <calebporzio@gmail.com>
2023-08-24 10:30:08 -0400

    Move history and navigate plugins into livewire/livewire untill they're stable (#3720)

--- packages/navigate/src/fetch.js
+++
@@ -1,8 +0,0 @@
-
-export function fetchHtml(destination, callback) {
-    let uri = destination.pathname + destination.search
-
-    fetch(uri).then(i => i.text()).then(html => {
-        callback(html)
-    })
-}

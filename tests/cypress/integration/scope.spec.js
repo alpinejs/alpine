6ed3551424a5a31630daca148b71f4d09d7605ce
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-10-12 09:14:25 +0400

    :test_tube: Adds additional test for stack direction

--- tests/cypress/integration/scope.spec.js
+++ tests/cypress/integration/scope.spec.js
@@ -11,6 +11,21 @@ test('properly merges the datastack',
     get('span').should(haveText('fizzbuzz'));
 });
 
+test('merges stack from bottom up', 
+    [html`
+        <div x-data="{ foo: 'fizz' }">
+            <div x-data="{ foo: 'buzz', get bar() { return this.foo } }">
+                <span id="one" x-text="bar + foo"></span>
+            </div>
+            <span id="two" x-text="foo"></span>
+        </div>
+    `]
+    , ({ get }) => {
+    get('span#one').should(haveText('fizzbuzz'));
+    get('span#two').should(haveText('fizz'));
+    }
+)
+
 test('handles getter setter pairs',
     [html`
         <div x-data="{ foo: 'fizzbuzz' }">

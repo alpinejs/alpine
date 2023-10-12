5ff69f7c8d9aea3b10511055a094b5a30192380d
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-10-12 09:08:16 +0400

    :bug: Fixes proxy stack setters bug

---
+++ tests/cypress/integration/scope.spec.js
@@ -0,0 +1,28 @@
+import { haveText, html, test } from "../utils";
+
+test('properly merges the datastack',
+    [html`
+        <div x-data="{ foo: 'fizz' }">
+            <div x-data="{ bar: 'buzz' }">
+                <span x-text="foo + bar"></span>
+            </div>
+        </div>
+    `], ({ get }) => {
+    get('span').should(haveText('fizzbuzz'));
+});
+
+test('handles getter setter pairs',
+    [html`
+        <div x-data="{ foo: 'fizzbuzz' }">
+            <div x-data="{ get bar() { return this.foo }, set bar(value) { this.foo = value } }">
+                <span id="one" x-text="bar" @click="bar = 'foobar'"></span>
+            </div>
+            <span id="two" x-text="foo"></span>
+        </div>
+    `], ({ get }) => {
+    get('span#one').should(haveText('fizzbuzz'));
+    get('span#two').should(haveText('fizzbuzz'));
+    get('span#one').click();
+    get('span#one').should(haveText('foobar'));
+    get('span#two').should(haveText('foobar'));
+});

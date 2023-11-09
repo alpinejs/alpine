4c57b81971f9cce2a450d547db69c78945868bc0
Caleb Porzio <calebporzio@gmail.com>
2023-11-09 13:51:52 -0500

    Skip x-effect during clone phase

--- packages/alpinejs/src/directives/x-effect.js
+++ packages/alpinejs/src/directives/x-effect.js
@@ -1,4 +1,6 @@
 import { directive } from '../directives'
-import { evaluateLater } from '../evaluator'
+import { evaluate, evaluateLater } from '../evaluator'
 
-directive('effect', (el, { expression }, { effect }) => effect(evaluateLater(el, expression)))
+directive('effect', skipDuringClone((el, { expression }, { effect }) => {
+    effect(evaluateLater(el,  expression))
+}))

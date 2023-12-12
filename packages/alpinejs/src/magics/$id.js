9c3d5272f80871c1d302a961a9dcdb18b103744d
Caleb Porzio <calebporzio@gmail.com>
2023-12-12 14:31:58 -0500

    fix x-id when used with morph

--- packages/alpinejs/src/magics/$id.js
+++ packages/alpinejs/src/magics/$id.js
@@ -1,14 +1,35 @@
 import { magic } from '../magics'
 import { closestIdRoot, findAndIncrementId } from '../ids'
+import { interceptClone } from '../clone'
 
-magic('id', el => (name, key = null) => {
+magic('id', (el, { cleanup }) => (name, key = null) => {
+    // We only want $id to run once per an element's lifecycle...
+    if (el._x_id) return el._x_id
+
     let root = closestIdRoot(el, name)
 
     let id = root
         ? root._x_ids[name]
         : findAndIncrementId(name)
 
-    return key
+    let output = key
         ? `${name}-${id}-${key}`
         : `${name}-${id}`
+
+    el._x_id = output
+
+    cleanup(() => {
+        delete el._x_id
+    })
+
+    return output
+})
+
+interceptClone((from, to) => {
+    // Transfer over existing ID registrations from
+    // the existing dom tree over to the new one
+    // so that there aren't ID mismatches...
+    if (from._x_id) {
+        to._x_id = from._x_id
+    }
 })

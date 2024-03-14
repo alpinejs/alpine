b867b6ebf4b0a611118d3b3fd820f0bd58bea7e4
Matt Vague <mattvague@gmail.com>
2024-03-14 11:58:01 -0700

    Throw error when trying to provide anything other than a function to Alpine.plugin

--- packages/alpinejs/src/plugin.js
+++ packages/alpinejs/src/plugin.js
@@ -3,5 +3,9 @@ import Alpine from "./alpine";
 export function plugin(callback) {
     let callbacks = Array.isArray(callback) ? callback : [callback]
 
-    callbacks.forEach(i => i(Alpine))
+    callbacks.forEach(i => {
+      if (typeof i === undefined) { throw new Error(`Alpine.plugin(...) expects a callback function but got undefined. Did you misspell your plugin import?`)}
+      if (typeof i !== 'function') { throw new Error(`Alpine.plugin(...) expects a callback function but got ${i}`)}
+      i(Alpine)
+    })
 }

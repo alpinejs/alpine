a9620fd6a1962c71ec9528825bded75baa752e94
Matt Vague <mattvague@gmail.com>
2024-03-15 10:20:03 -0700

    Remove unnecessary error message

--- packages/alpinejs/src/plugin.js
+++ packages/alpinejs/src/plugin.js
@@ -4,7 +4,6 @@ export function plugin(callback) {
     let callbacks = Array.isArray(callback) ? callback : [callback]
 
     callbacks.forEach(i => {
-      if (typeof i === undefined) { throw new Error(`Alpine.plugin(...) expects a callback function but got undefined. Did you misspell your plugin import?`)}
       if (typeof i !== 'function') { throw new Error(`Alpine.plugin(...) expects a callback function but got ${i}`)}
       i(Alpine)
     })

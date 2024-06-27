76899f9217d87e6356e287482eeccab3664a83dc
abhi <abhi9848038283@gmail.com>
2024-06-27 15:58:00 +0545

    add return comment

--- packages/alpinejs/src/utils/get-utlilties.js
+++ packages/alpinejs/src/utils/get-utlilties.js
@@ -2,8 +2,9 @@ import { getElementBoundUtilities } from "../directives";
 import { interceptor } from "../interceptor";
 import { onElRemoved } from "../mutation";
 
-/// Get the utilities for a given element.
+/// Gets the utilities for a given element.
 /// @param el - The element to get the utilities for.
+/// @returns The utilities for the given element.
 export function getUtilities(el) {
     let [utilities, cleanup] = getElementBoundUtilities(el)
     const utils = { interceptor, ...utilities }

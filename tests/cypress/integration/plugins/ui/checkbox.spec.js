df1f3c5689fe2fa0337c15f16161c86066d453f0
Günther Debrauwer <gunther@nextapps.be>
2024-07-23 11:23:54 +0200

    Update checkbox.spec.js

--- tests/cypress/integration/plugins/ui/checkbox.spec.js
+++ tests/cypress/integration/plugins/ui/checkbox.spec.js
@@ -9,7 +9,7 @@ test('it works without group using x-model',
             </div>
 
             <input x-model="active" type="hidden">
-            <button x-on:click="active = !active>Toggle</button>
+            <button x-on:click="active = !active">Toggle</button>
         </main>
     `],
     ({ get }) => {
@@ -24,5 +24,39 @@ test('it works without group using x-model',
         get('button').click()
         get('input').should(haveAttribute('value', 'false'))
         get('[x-checkbox]').should(haveAttribute('aria-checked', 'false'))
+    },
+)
+
+test('it works with group using x-model',
+    [html`
+        <main x-data="{ colors: [] }">
+            <div x-checkbox:group x-model="colors">
+                <div x-checkbox value="red">
+                    <span x-checkbox:label>Red</span>
+                </div>
+                <div x-checkbox value="blue">
+                    <span x-checkbox:label>Blue</span>
+                </div>
+                <div x-checkbox value="green">
+                    <span x-checkbox:label>Green</span>
+                </div>
+            </div>
+
+            <input x-model="colors" type="hidden">
+            <button data-color="red" x-on:click="(index = colors.findIndex(color => color === 'red')) > 0 ? colors.splice(index, 1)" : colors.push('red')">Toggle Red</button>
+            <button data-color="blue" x-on:click="(index = colors.findIndex(color => color === 'blue')) > 0 ? colors.splice(index, 1)" : colors.push('blue')">Toggle Blue</button>
+            <button data-color="green" x-on:click="(index = colors.findIndex(color => color === 'green')) > 0 ? colors.splice(index, 1)" : colors.push('green')">Toggle Green</button>
+        </main>
+    `],
+    ({ get }) => {
+        get('input').should(haveAttribute('value', ''))
+        get('[value="red"]').click()
+        get('input').should(haveAttribute('value', 'red'))
+        get('[value="green"]').click()
+        get('input').should(haveAttribute('value', 'red,green'))
+        get('[value="blue"]').click()
+        get('input').should(haveAttribute('value', 'red,green,blue'))
+        get('[value="green"]').click()
+        get('input').should(haveAttribute('value', 'red,blue'))
     },
 )

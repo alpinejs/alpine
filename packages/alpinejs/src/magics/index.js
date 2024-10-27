6083971721c7823d5e5503ddcc195ec55ac0449e
MaquinaTech <nicolopezdelerma@gmail.com>
2024-10-27 20:02:40 +0100

    add-use-state: Creare useState magic

--- packages/alpinejs/src/magics/index.js
+++ packages/alpinejs/src/magics/index.js
@@ -1,6 +1,7 @@
 import { warn } from '../utils/warn'
 import { magic } from '../magics'
 
+import './$useState'
 import './$nextTick'
 import './$dispatch'
 import './$watch'
@@ -14,6 +15,7 @@ import './$el'
 // Register warnings for people using plugin syntaxes and not loading the plugin itself:
 warnMissingPluginMagic('Focus', 'focus', 'focus')
 warnMissingPluginMagic('Persist', 'persist', 'persist')
+warnMissingPluginMagic('useState()', 'useState', 'use-state')
 
 function warnMissingPluginMagic(name, magicName, slug) {
     magic(magicName, (el) => warn(`You can't use [$${magicName}] without first installing the "${name}" plugin here: https://alpinejs.dev/plugins/${slug}`, el))

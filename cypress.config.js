4fad41469ec94b5c45481b4d5ec62a3b1a39f938
Matt Vague <mattvague@gmail.com>
2024-10-28 09:16:19 -0700

    Use correct support file

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     specPattern: 'tests/cypress/**/*.spec.js',
+    supportFile: 'tests/cypress/support/index.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },

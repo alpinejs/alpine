0e5f1be30f5b210491a5fdd2038a98fc94bb4f64
Caleb Porzio <calebporzio@gmail.com>
2026-04-01 10:58:13 -0400

    Add anchor `.noflip` modifier (#4791)
    Co-authored-by: antoine <antoine.guingand@wanadoo.fr>

--- tests/cypress/integration/plugins/anchor.spec.js
+++ tests/cypress/integration/plugins/anchor.spec.js
@@ -34,3 +34,26 @@ test('can anchor to dynamic reference',
         })
     }
 )
+
+test('noflip modifier prevents automatic flipping',
+    [html`
+        <div x-data style="height: 100vh; display: flex; align-items: flex-end;">
+            <button x-ref="foo" style="margin-top: auto;">toggle</button>
+            <div x-anchor.bottom.noflip="$refs.foo" id="anchored">dropdown</div>
+        </div>
+    `],
+    ({ get }, reload) => {
+        get('#anchored').should(haveComputedStyle('position', 'absolute'))
+
+        // The element should be positioned below the button (bottom placement)
+        // even though there's no room — noflip prevents it from flipping to top
+        get('button').then(($btn) => {
+            let btnBottom = $btn[0].getBoundingClientRect().bottom
+
+            get('#anchored').then(($el) => {
+                let elTop = $el[0].getBoundingClientRect().top
+                expect(elTop).to.be.gte(btnBottom - 1)
+            })
+        })
+    },
+)

9e501938972f5aea26a747d7d8c57c1755fdde93
Josh Hanley <josh@hitsystems.com.au>
2026-07-01 10:59:41 +1000

    Test intersect margin with parent root

--- tests/cypress/integration/plugins/intersect.spec.js
+++ tests/cypress/integration/plugins/intersect.spec.js
@@ -128,22 +128,22 @@ test.only('.margin',
     [html`
     <div x-data="{ count: 0 }">
         <span x-text="count"></span>
-        <div id="buffer-top" style="height: calc(100vh - 50px); margin-top: 100vh; background: pink"></div>
-        <div id="buffer-bottom" style="height: 50px; background: green"></div>
-        <div x-intersect.margin.200px="count++;$nextTick(() => console.log(count))" id="1">hi</div>
+        <div id="container" style="height: 200px; overflow-y: scroll;">
+            <div style="height: 350px;">spacer</div>
+            <div x-intersect.parent.margin.100px="count++" id="1" style="height: 20px;">hi</div>
+            <div style="height: 300px;"></div>
+        </div>
     </div>
     `],
     ({ get }) => {
         get('span').should(haveText('0'))
-        get('#buffer-top').scrollIntoView({duration: 100})
+        get('#container').scrollTo(0, 75, {duration: 100})
         get('span').should(haveText('1'))
-        get('#1').scrollIntoView({duration: 100})
+        get('#container').scrollTo(0, 200, {duration: 100})
         get('span').should(haveText('1'))
-        get('span').scrollIntoView({duration: 100})
+        get('#container').scrollTo(0, 0, {duration: 100})
         get('span').should(haveText('1'))
-        get('#buffer-top').scrollIntoView({duration: 100})
-        get('span').should(haveText('2'))
-        get('#1').scrollIntoView({duration: 100})
+        get('#container').scrollTo(0, 75, {duration: 100})
         get('span').should(haveText('2'))
     },
 )
